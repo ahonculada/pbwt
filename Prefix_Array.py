91af65527001fe52f6fe3ffd05e0cee355f3a4dc
aaron <aaron@pop-os.localdomain>
2022-03-21 18:46:48 -0400

    print all steps in sequence length

--- Prefix_Array.py
+++ Prefix_Array.py
@@ -9,46 +9,90 @@ class Prefix_Array:
         self.num_seq = haplotypes.rows
         self.seq_len = haplotypes.cols
         self.snapshot_default()
-        self.prefix_array = self.BuildPrefixArray()
+        self.prefix_array, self.div_array = self.BuildPrefixArray()
+        print(self.div_array)
+        self.print_final()
 
     # Algorithm 1
-    def BuildPrefixArray(self) -> List[List[int]]:
+    def BuildPrefixArray(self) -> (list, list):
         # initialize positional prefix array
         PPA = list(range(self.num_seq))
+        DIV = [0 for _ in range(self.num_seq)]
         for k in range(self.seq_len-1):
             a, b = [], []
-            for idx in PPA:
+            d, e = [], []
+            p, q = k + 1, k + 1
+            for idx, match_start in zip(PPA, DIV):
                 haplotype = self.haplotypes[idx]
                 allele = haplotype[k]
+                if match_start > p:
+                    p = match_start
+                if match_start > q:
+                    q = match_start
                 if not allele:
                     a.append(idx)
+                    d.append(p)
+                    p = 0
                 else:
                     b.append(idx)
+                    e.append(q)
+                    q = 0
             a.extend(b)
+            d.extend(e)
             PPA = a
+            DIV = d
             # print PPA of current timestep
-            self.snapshot(PPA)
-        return PPA
+            self.snapshot(PPA, DIV, k)
+        return PPA, DIV
 
     # Build HTML for display
     def snapshot_default(self):
         html = '<pre style="line-height: 100%">'
         for idx in range(self.num_seq):
             haplotype = self.haplotypes[idx]
-            html += str(idx) + '|' + ''.join(str(allele) for allele in haplotype[:-1]) + '</br>'
+            html += str(idx) + '|' + ''.join(str(allele) for allele in haplotype) + '</br>'
         html += '</pre>'
         with open('/tmp/sample.html', 'a') as sample:
             sample.write(html)
 
     # Build HTML for display
-    def snapshot(self, PPA: list):
+    def snapshot(self, PPA: list, DIV: list, step: int):
         html = '<pre style="line-height: 100%">'
-        for idx in PPA:
+        for idx, match_start in zip(PPA, DIV):
             haplotype = self.haplotypes[idx]
-            html += str(idx) + '|' + ''.join(str(allele) for allele in haplotype[:-1]) + '</br>'
+            html += str(idx) + '|'
+            for k, allele in enumerate(haplotype[:step+1]):
+                if match_start == k:
+                    html += '<strong><u>'
+                html += str(allele)
+            if match_start < len(haplotype) - 1:
+                html += '</u></strong>'
+            html += '  ' + str(haplotype[step+1]) + '  ' 
+            for k, allele in enumerate(haplotype[step+2:]):
+                html += str(allele)
+            html += '<br/>'
+
         html += '</pre>'
         with open('/tmp/sample.html', 'a') as sample:
             sample.write(html)
+
+    def print_final(self):
+        html = '<pre style="line-height: 100%">'
+        for i, pair in enumerate(zip(self.prefix_array, self.div_array)):
+            idx, match_start = pair
+            haplotype = self.haplotypes[idx]
+            html += str(idx) + '|'
+            for k, allele in enumerate(haplotype):
+                if match_start == k and i:
+                    html += '<strong><u>'
+                html += str(allele)
+            html += '</u></strong>'
+            html += '<br/>'
+        html += '</pre>'
+        with open('/tmp/sample.html', 'a') as sample:
+            sample.write(html)
+
+
 
 
 if __name__ == '__main__':
